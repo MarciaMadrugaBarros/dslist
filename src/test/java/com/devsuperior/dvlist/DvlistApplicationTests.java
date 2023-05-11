fc88a6f368cde2697e77d16eda41e020958ca298
MarciaMadrugaBarros <marcinha_madruga@hotmail.com>
2023-05-11 12:04:55 -0300

    Project created

---
+++ src/test/java/com/devsuperior/dvlist/DvlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dvlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
